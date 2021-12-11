15c9fe22f9d2e95ee74db9410e5f31cb1149890c
avilind <xavilind7@gmail.com>
2021-12-11 17:32:16 -0600

    InsertarRegistroSegundoPlano

--- app/src/main/java/com/example/stores/MainActivity.kt
+++ app/src/main/java/com/example/stores/MainActivity.kt
@@ -19,6 +19,10 @@ class MainActivity : AppCompatActivity(), OnClickListener{
 
         mBinding.btnSave.setOnClickListener {
             val store = StoreEntity(name = mBinding.etName.text.toString().trim())
+            Thread {
+            StoreApplication.database.storeDao().addStore(store)
+            }.start()
+
             mAdapter.add(store)
         }
 
