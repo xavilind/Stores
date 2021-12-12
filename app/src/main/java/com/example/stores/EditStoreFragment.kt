0c4e1d44306f9a6cf191f7d59088e23b6f6ca562
avilind <xavilind7@gmail.com>
2021-12-12 00:12:14 -0600

    InterfaceFragment

--- app/src/main/java/com/example/stores/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores/EditStoreFragment.kt
@@ -1,10 +1,15 @@
 package com.example.stores
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import com.example.stores.databinding.FragmentEditStoreBinding
 import com.google.android.material.snackbar.Snackbar
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
 
 class EditStoreFragment : Fragment() {
 
@@ -40,13 +45,43 @@ class EditStoreFragment : Fragment() {
                 true
             }
             R.id.action_save -> {
-                Snackbar.make(mBiding.root, getString(R.string.edit_store_message_save_success),
-                    Snackbar.LENGTH_SHORT)
-                    .show()
+
+                val store = StoreEntity(name = mBiding.etName.text.toString().trim(),
+                    phone = mBiding.etPhone.text.toString().trim(),
+                    website = mBiding.etWebsite.text.toString().trim())
+
+                doAsync {
+                    StoreApplication.database.storeDao().addStore(store)
+                    uiThread {
+                        mActivity?.addStore(store)
+
+                        hideKeyboard()
+
+                        Snackbar.make(mBiding.root,
+                            getString(R.string.edit_store_message_save_success),
+                            Snackbar.LENGTH_SHORT)
+                            .show()
+
+                        mActivity?.onBackPressed()
+                    }
+                }
+
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun hideKeyboard(){
+        val imm = mActivity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (view != null){
+            imm.hideSoftInputFromWindow(view!!.windowToken, 0)
+        }
+    }
+
+    override fun onDestroyView() {
+        hideKeyboard()
+        super.onDestroyView()
     }
 
     override fun onDestroy() {
