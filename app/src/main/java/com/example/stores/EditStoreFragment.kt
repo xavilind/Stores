d3df028781554641cab26c1f79ab655e91d11b5f
avilind <xavilind7@gmail.com>
2021-12-11 22:55:36 -0600

    SeleccionMenu

--- app/src/main/java/com/example/stores/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores/EditStoreFragment.kt
@@ -1,15 +1,15 @@
 package com.example.stores
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.stores.databinding.FragmentEditStoreBinding
+import com.google.android.material.snackbar.Snackbar
 
 class EditStoreFragment : Fragment() {
 
     private lateinit var mBiding: FragmentEditStoreBinding
+    private var mActivity: MainActivity? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle? ): View? {
@@ -21,9 +21,32 @@ class EditStoreFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val activity = activity as? MainActivity
-        activity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        activity?.supportActionBar?.title = getString(R.string.edit_store_title_add)
+        mActivity = activity as? MainActivity
+        mActivity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mActivity?.supportActionBar?.title = getString(R.string.edit_store_title_add)
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_save, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                mActivity?.onBackPressed()
+                true
+            }
+            R.id.action_save -> {
+                Snackbar.make(mBiding.root, getString(R.string.edit_store_message_save_success),
+                    Snackbar.LENGTH_SHORT)
+                    .show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
